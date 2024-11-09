a60ec477d8bc5ee26e49c9754812429c721804f7
beuri97 <beuri97@gmail.com>
2024-11-09 15:12:55 +1300

    initial commit

---
+++ src/main/java/me/h_yang/my_homepage/MyHomepageApplication.java
@@ -0,0 +1,13 @@
+package me.h_yang.my_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/h_yang/my_homepage/MyHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.h_yang.my_homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
