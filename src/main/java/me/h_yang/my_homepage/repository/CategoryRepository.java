5d7a55a57caf2f5751c5b7507cb2b7c072a7ced2
beuri97 <beuri97@gmail.com>
2024-11-27 22:34:59 +1300

    feat: added category features
    - added category service and repository
    - added Category and SubCategory Entities

---
+++ src/main/java/me/h_yang/my_homepage/repository/CategoryRepository.java
@@ -0,0 +1,27 @@
+package me.h_yang.my_homepage.repository;
+
+import me.h_yang.my_homepage.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    /**
+     * Find all categories
+     *
+     * @return all categories
+     */
+    List<Category> findAll();
+
+    /**
+     * Find category by category title
+     *
+     * @param categoryTitle Category title
+     * @return Category object
+     */
+    Category findByCategoryTitle(String categoryTitle);
+
+}
---
+++ src/main/java/me/h_yang/my_homepage/service/CategoryService.java
@@ -0,0 +1,44 @@
+package me.h_yang.my_homepage.service;
+
+import me.h_yang.my_homepage.entity.Category;
+import me.h_yang.my_homepage.entity.SubCategory;
+import me.h_yang.my_homepage.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    /**
+     * Get all exist categories
+     *
+     * @return list of Category titles
+     */
+    public List<String> getAllCategoriesName() {
+
+        return categoryRepository.findAll().stream().map(Category::getCategoryTitle).toList();
+    }
+
+    /**
+     * Get all subcategories of a category
+     *
+     * @param categoryTitle the title of the category
+     * @return list of subcategory titles
+     */
+    public List<String> getSubCategoriesName(String categoryTitle) {
+
+        return categoryRepository.findByCategoryTitle(categoryTitle)
+                .getSubCategoryList().stream().map(SubCategory::getSubCategoryTitle).toList();
+    }
+}
