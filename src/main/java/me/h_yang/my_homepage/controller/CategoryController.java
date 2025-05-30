0333c819317bc7ce6f18bcf1bcc0c4f3dbc9611d
Astro-Luminoso <beuri97@gmail.com>
2025-05-31 00:17:23 +1200

    feat: Created Logger into CategoryController

--- src/main/java/me/h_yang/my_homepage/controller/CategoryController.java
+++ src/main/java/me/h_yang/my_homepage/controller/CategoryController.java
@@ -4,6 +4,8 @@ package me.h_yang.my_homepage.controller;
 import me.h_yang.my_homepage.dto.CategoryDTO;
 import me.h_yang.my_homepage.dto.SubCategoryDTO;
 import me.h_yang.my_homepage.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,8 @@ public class CategoryController {
     @Autowired
     public CategoryService categoryService;
 
+    Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
     /**
      * Get all categories
      *
@@ -26,11 +30,17 @@ public class CategoryController {
      */
     @GetMapping
     public List<CategoryDTO> getAllCategories() {
+
+        logger.info("GET: /open/categories - get all categories");
+
         return categoryService.getAllCategories();
     }
 
     @GetMapping("/subcategories")
     public List<SubCategoryDTO> getAllSubCategoriesByCategoryId(@RequestParam("categoryId") long categoryId) {
+
+        logger.info("GET: /open/categories/subcategories - get all subcategories for categoryId: {}", categoryId);
+
         return categoryService.getAllSubCategoriesByCategoryId(categoryId);
     }
 }
