77e2f86b3115465fde85893e20caa83175b11c38
Astro-Luminoso <beuri97@gmail.com>
2025-07-28 18:02:30 +0900

    feat: updated BlogPostRepository to apply specification and pageable

--- src/main/java/me/h_yang/my_homepage/repository/BlogPostRepository.java
+++ src/main/java/me/h_yang/my_homepage/repository/BlogPostRepository.java
@@ -2,13 +2,19 @@ package me.h_yang.my_homepage.repository;
 
 import me.h_yang.my_homepage.entity.BlogPost;
 import me.h_yang.my_homepage.entity.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long>, JpaSpecificationExecutor<BlogPost> {
 
     List<BlogPost> findAllByOrderByUpdatedDateDesc();
 
     List<BlogPost> findAllByCategoryOrderByUpdatedDateDesc(Category category);
+
+    Page<BlogPost> findAll(Specification<BlogPost> spec, Pageable pageable);
 }
