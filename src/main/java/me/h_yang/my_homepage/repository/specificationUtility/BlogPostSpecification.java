a1e31be5f97beb8fc5df4b6740a51aa6d5645870
Astro-Luminoso <beuri97@gmail.com>
2025-07-28 18:01:55 +0900

    feat: Created BlogPostSpecification to filter db query

---
+++ src/main/java/me/h_yang/my_homepage/repository/specificationUtility/BlogPostSpecification.java
@@ -0,0 +1,18 @@
+package me.h_yang.my_homepage.repository.specificationUtility;
+
+import me.h_yang.my_homepage.entity.BlogPost;
+import org.springframework.data.jpa.domain.Specification;
+
+public class BlogPostSpecification {
+
+    public static Specification<BlogPost> hasTitle(String title) {
+
+        return title == null ?
+                null : (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("title"), title);
+    }
+
+    public static Specification<BlogPost> hasCategoryId(Long categoryId) {
+        return categoryId == null ?
+                null : (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("category"), categoryId);
+    }
+}
