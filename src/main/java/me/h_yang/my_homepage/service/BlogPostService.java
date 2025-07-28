5f2b421d9f305f909049b4a97916f9f9e706e4d9
Astro-Luminoso <beuri97@gmail.com>
2025-07-28 18:54:38 +0900

    feat: added logger in BlogPostService.java

--- src/main/java/me/h_yang/my_homepage/service/BlogPostService.java
+++ src/main/java/me/h_yang/my_homepage/service/BlogPostService.java
@@ -4,6 +4,8 @@ import me.h_yang.my_homepage.dto.BriefBlogPostDTO;
 import me.h_yang.my_homepage.entity.BlogPost;
 import me.h_yang.my_homepage.repository.BlogPostRepository;
 import me.h_yang.my_homepage.repository.specificationUtility.BlogPostSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +21,8 @@ public class BlogPostService {
 
     BlogPostRepository blogPostRepository;
 
+    Logger logger = LoggerFactory.getLogger(BlogPostService.class);
+
     @Autowired
     public BlogPostService(BlogPostRepository blogPostRepository) {
         this.blogPostRepository = blogPostRepository;
@@ -27,12 +31,17 @@ public class BlogPostService {
 
     public List <BriefBlogPostDTO> getBlogPostBriefByPage (String title, Long categoryId, int page, int size) {
 
+        logger.info("BLOG POST SERVICE: Manipulating WHERE clause for blog post search with title: {}, categoryId: {}",
+                title, categoryId);
+
         Specification<BlogPost> spec = Specification
                 .where(BlogPostSpecification.hasTitle(title))
                 .and(BlogPostSpecification.hasCategoryId(categoryId));
+        logger.info("BLOG POST SERVICE: The Where clause is created: {}", spec);
 
         Pageable pageable = PageRequest.of(page, size, Sort.by("updatedDate").descending());
 
+        logger.info("BLOG POST SERVICE: Retrieving data");
         return blogPostRepository.findAll(spec, pageable)
                 .stream()
                 .map(post -> new BriefBlogPostDTO(
