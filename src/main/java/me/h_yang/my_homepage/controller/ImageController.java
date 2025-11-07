c669e59ca6ebb639fbbcd05593c6bb07ce7262c8
lambda_luna <beuri97@gmail.com>
2025-11-07 12:01:10 +0900

    fix: logger typo
    - removed unused import statement

--- src/main/java/me/h_yang/my_homepage/controller/ImageController.java
+++ src/main/java/me/h_yang/my_homepage/controller/ImageController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/open/images")
