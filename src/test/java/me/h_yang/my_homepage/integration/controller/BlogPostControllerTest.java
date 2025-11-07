9b23de8a3e220cc228e4ed8eb650eae551a40106
lambda_luna <beuri97@gmail.com>
2025-11-07 16:18:15 +0900

    feature: created new controller test
    -skeleton file nothing is implemented yet

---
+++ src/test/java/me/h_yang/my_homepage/integration/controller/BlogPostControllerTest.java
@@ -0,0 +1,9 @@
+package me.h_yang.my_homepage.integration.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BlogPostControllerTest {
+
+
+}
